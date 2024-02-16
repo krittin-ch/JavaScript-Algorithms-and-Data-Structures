725fbb69bd3053491df5c9f68c79f741a6e89426
Krittin Chaowakarn <130139478+krittin-ch@users.noreply.github.com>
2024-02-16 11:00:06 +0700

    Upload

--- Telephone_Number_Validator/app.js
+++ Telephone_Number_Validator/app.js
@@ -1,3 +1,8 @@
+const checkBtn = document.getElementById("check-btn")
+const clearBtn = document.getElementById("clear-btn")
+const userInput = document.getElementById("user-input")
+const resultDiv = document.getElementById("result-div")
+
 let A = [
     '1 555-555-5555',
     '1 (555) 555-5555',
@@ -8,20 +13,50 @@ let A = [
     '(555)555-5555'
 ]
 
-function number(num) {
-    return num.replace(/[^0-9]/g, '')
-}
-
-function numValidate(num) {
-    if (num.length == 10) {
-        return true
-    } else if (num.length == 11 && num[0] == 1) {
-        return true
-    } else {
-        return false
+const checkValidNumber = input => {
+    if (input === '') {
+      alert('Please provide a phone number')
+      return
     }
+
+    const countryCode = '^(1\\s?)?'
+    const areaCode = '(\\([0-9]{3}\\)|[0-9]{3})'
+    const spaceDash = '(-\\s?)?'
+    const num1 = '[0-9]{3}'
+    const num2 = '[0-9]{4}$'
+    const phoneRegEx = new RegExp(`${countryCode}${areaCode}${spaceDash}${num1}${spaceDash}${num2}`)
+
+    const pTag = document.createElement('p');
+    pTag.className = 'result-text' // create class to get the same CSS styl
+    
+    phoneRegEx.test(input) 
+    ? (
+        pTag.style.color = 'green',
+        pTag.innerHTML = `Valid US number: ${input}`
+    )
+    : (
+        pTag.style.color = 'red',
+        pTag.innerHTML = `Invalid US number: ${input}`
+    )
+
+    resultDiv.appendChild(pTag)
 }
 
-let B = number(A[0])
-console.log(A[0])
-console.log(B)
+checkBtn.addEventListener("click", () => {
+    checkValidNumber(userInput.value)
+    console.log(userInput.value)
+}
+)
+
+
+
+
+
+
+
+
+// '^(1//s?)?'
+// '(\\[0-9]{3}\\)|[0-9]{3}'
+// '(-\\s?)?'
+// '[0-9]{3}'
+// '[0-9]{4}'
